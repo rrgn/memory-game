ceec298c4448df1cb77b44ff6348ac15daa4ae0f
Regan <regs.co@gmail.com>
2016-05-19 10:52:54 -0400

    added css animation

--- MEmGame.js
+++ MEmGame.js
@@ -1,45 +1,40 @@
 //function for clicking image
-var firstMon = "";
-var secondMon = "";
 var state = "turn1"
 
+function match() {
+  var openTiles = $('.show img');
+  var first = $(openTiles[0]);
+  var second = $(openTiles[1]);
+  return first.attr('src') === second.attr('src');
+}
 
 $(function() {
   $('.tile').click(function() {
-    $(this).addClass('show');
-  });
-
-  $('.tile').click(function() {
+    if ($(this).hasClass('show animated flipInY')) {
+      return;
+    }
     if (state === "turn1") {
+      $(this).addClass('show animated flipInY');
       state = "turn2";
-    } else {
+    } else if (state === "turn2") {
+      $(this).addClass('show animated flipInY');
       state = "turn1";
-    }
-    if (state === "turn2") {
-      secondMon = $(this).find('.monster').attr('src');
-    }
-    else if (state === "turn1") {
-      firstMon = $(this).find('.monster').attr('src');
+
+    if (match()) {
+      $('.show').removeClass('show')
+        .addClass('match');
+    } else {
+      setTimeout(function() {
+        $('.tile').removeClass('show');
+      }, 1000);
     }
 
-    if (state === "turn1" && firstMon === secondMon) {
-      $('.tile.show').addClass('match');
-      $('.tile.show').addClass('match');
-    } else if (state === "turn1") {
-      setTimeout(function() {
-        $('.tile').removeClass('show')}, 1000);
-    }
-    var matched = $('.matched').length;
+  }
+    var matched = $('.match').length;
     var totalCards = $('.tile').length;
     if (matched === totalCards) {
       $('#space').text("You Win!");
     }
+
   });
 });
-
-/*
-var matched = $('.matched').length;
-var totalCards = $('.tile').length;
-if (matched === totalCards) {
-  $('#messages').text("You Win!");
-} */
